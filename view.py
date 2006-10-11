dc6a77ce4480881d9d5e076b312bf63c437d9acd
Richard Waid <richard@iopen.net>
2006-10-11 02:05:57 +0000

    Error pages
    --HG--
    branch : GSContent
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSContent%401304

--- view.py
+++ view.py
@@ -5,10 +5,25 @@ hoopy later.
 '''
 import sys, re, datetime, time
 import Products.Five, DateTime, Globals
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
 class GSView(Products.Five.BrowserView):
     pass
     
     # First one to be converted: Scripts.get_firstLevelFolder(context)
-    
+
+class GSNotFoundError(Products.Five.BrowserView):
+    index = ZopeTwoPageTemplateFile('browser/templates/not_found.pt')
+    # make the template publishable
+    def __call__(self, *args, **kw):
+        self.request.response.setStatus(404)
+        return self.index(self, *args, **kw)
+
+class GSUnknownError(Products.Five.BrowserView):
+   index = ZopeTwoPageTemplateFile('browser/templates/unknown_error.pt')
+   def __call__(self, *args, **kw):
+       # should this really be a 500, that suggests a server error?
+       #self.request.response.setStatus(500)
+       return self.index(self, *args, **kw)
+
 Globals.InitializeClass( GSView )
