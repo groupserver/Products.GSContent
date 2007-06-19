cea46523de43da71ff59205f1fec1d8e11accbed
Michael JasonSmith <mpj17@onlinegroups.net>
2007-06-19 05:55:32 +0000

    Adding Prototype to all pages, along with disclosure button support. Complete rewite of the disclosure button API.
    --HG--
    branch : GSContent
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSContent%402156

--- browser/javascript/disclosure_button/disclosure_toggle.js
+++ browser/javascript/disclosure_button/disclosure_toggle.js
@@ -1,17 +1,77 @@
-function showHideDisclosure(remainderId, discloseButtonId, text)
+// Functions for handling the disclosure buttons.
+var disclosureHiddenArrow = "\u25b6";
+var disclosureShownArrow = "\u25bc";
+var disclosureNBSP = "\u00a0";
+
+function showHideDisclosure(widget)
 {
-    var hiddenArrow = "\u25b6";
-    var shownArrow = "\u25bc";
-    var nbsp = "\u00a0";
-    var discloseElement = document.getElementById(discloseButtonId);
-    if (discloseElement.childNodes[0].data.substring(0,1) == shownArrow)
-    { 
-        discloseElement.childNodes[0].data = hiddenArrow + nbsp + text;
+    var w = $(widget);
+    setToggleArrow(w);
+    showhide = w.getElementsByClassName('disclosureShowHide')[0];
+    Effect.toggle(showhide, 'blind', {duration: 1, delay: 0});
+}
+
+function addArrowsToDisclosureButtons()
+{
+    var i = 0;
+    widgets = document.getElementsByClassName('disclosureWidget');
+    for (i=0; i< widgets.length; i++)
+    {
+        button = widgets[i].getElementsByClassName('disclosureButton')[0];
+        addHiddenArrowToElement(button);
+    }
+}
+
+function setToggleArrow(w)
+{
+    button = $(w).getElementsByClassName('disclosureButton')[0];
+    if (widgetHidden(w))
+    {
+        addShownArrowToElement(button);
     }
     else
     {
-        discloseElement.childNodes[0].data = shownArrow + nbsp + text;
+        addHiddenArrowToElement(button);
     }
-    Effect.toggle(remainderId,'blind', {duration: 1, delay: 0});
 }
+
+function widgetHidden(widget)
+{
+    var retval = true;
+    var w = $(widget);
+    var showhide = w.getElementsByClassName('disclosureShowHide')[0];
+    var showhideDisplay = showhide.getStyle('display');
+    retval = (showhideDisplay == 'none');
+    return retval;
+}
+
+function addShownArrowToElement(element)
+{
+    var e = $(element);
+    var text  = getRealText(e.childNodes[0].data);
+    e.childNodes[0].data = disclosureShownArrow + disclosureNBSP + text;
+}
+
+function addHiddenArrowToElement(element)
+{
+    var e = $(element);
+    var text  = getRealText(e.childNodes[0].data);
+    e.childNodes[0].data = disclosureHiddenArrow + disclosureNBSP + text;
+}
+
+function getRealText(text)
+{
+    var retval = '';
+    c0 = text.charAt(0);
+    if ((c0 == disclosureHiddenArrow) || (c0 == disclosureShownArrow))
+    {
+        retval = text.substring(2, text.length);
+    }
+    else
+    {
+        retval = text;
+    }
+    return retval;
+}
+
 
