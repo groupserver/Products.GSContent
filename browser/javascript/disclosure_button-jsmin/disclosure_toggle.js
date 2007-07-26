456a9df6f812edbec41ea835f5b88bcec70ab049
Michael JasonSmith <mpj17@onlinegroups.net>
2007-07-26 05:26:18 +0000

    Adding versions of the main JavaScript files, to see if running them through "jsmin" helps performance.
    --HG--
    branch : GSContent
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSContent%402339

---
+++ browser/javascript/disclosure_button-jsmin/disclosure_toggle.js
@@ -0,0 +1,24 @@
+
+var disclosureHiddenArrow="\u25b6";var disclosureShownArrow="\u25bc";var disclosureNBSP="\u00a0";function showHideDisclosure(widget)
+{var w=$(widget);setToggleArrow(w);showhide=w.getElementsByClassName('disclosureShowHide')[0];Effect.toggle(showhide,'blind',{duration:1,delay:0});}
+function addArrowsToDisclosureButtons()
+{var i=0;widgets=document.getElementsByClassName('disclosureWidget');for(i=0;i<widgets.length;i++)
+{button=widgets[i].getElementsByClassName('disclosureButton')[0];addHiddenArrowToElement(button);}}
+function setToggleArrow(w)
+{button=$(w).getElementsByClassName('disclosureButton')[0];if(widgetHidden(w))
+{addShownArrowToElement(button);}
+else
+{addHiddenArrowToElement(button);}}
+function widgetHidden(widget)
+{var retval=true;var w=$(widget);var showhide=w.getElementsByClassName('disclosureShowHide')[0];var showhideDisplay=showhide.getStyle('display');retval=(showhideDisplay=='none');return retval;}
+function addShownArrowToElement(element)
+{var e=$(element);var text=getRealText(e.childNodes[0].data);e.childNodes[0].data=disclosureShownArrow+disclosureNBSP+text;}
+function addHiddenArrowToElement(element)
+{var e=$(element);var text=getRealText(e.childNodes[0].data);e.childNodes[0].data=disclosureHiddenArrow+disclosureNBSP+text;}
+function getRealText(text)
+{var retval='';c0=text.charAt(0);if((c0==disclosureHiddenArrow)||(c0==disclosureShownArrow))
+{retval=text.substring(2,text.length);}
+else
+{retval=text;}
+return retval;}
+Event.observe(window,'load',addArrowsToDisclosureButtons);
