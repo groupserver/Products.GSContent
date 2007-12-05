1330d4c2ba7ce713b5b1c9e8321d5769fa7518bf
Michael JasonSmith <mpj17@onlinegroups.net>
2007-12-05 04:37:06 +0000

    Added code to check to determine the visible group-memberships, without breaching too much privacy.
    --HG--
    branch : GSContent
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSContent%402885

--- groupsInfo.py
+++ groupsInfo.py
@@ -7,6 +7,7 @@ from zope.app.folder.interfaces import IFolder
 from zope.component.interfaces import IFactory
 
 import AccessControl
+from Products.GSGroup.queries import GroupQuery
 
 class GSGroupsInfoFactory(object):
     implements(IFactory)
@@ -35,6 +36,10 @@ class GSGroupsInfo(object):
         
         self.__allGroups = None
         self.__visibleGroups = None
+        self.folderTypes = ['Folder', 'Folder (Ordered)']
+
+        self.da = context.zsqlalchemy
+        self.groupQuery = GroupQuery(context, self.da)
         
     def __get_groups_object(self):
         assert self.siteInfo, 'Site Info is set to %s' % self.siteInfo
@@ -56,9 +61,9 @@ class GSGroupsInfo(object):
     def get_all_groups(self):
         assert self.groupsObj
         if self.__allGroups == None:
-            folderTypes = ['Folder', 'Folder (Ordered)']
-            allGroups = [g for g in self.groupsObj.objectValues(folderTypes)
-                          if g.getProperty('is_group', False)]
+            allGroups = [g for g in \
+                         self.groupsObj.objectValues(self.folderTypes)
+                         if g.getProperty('is_group', False)]
             self.__allGroups = allGroups
             
         return self.__allGroups
@@ -87,4 +92,82 @@ class GSGroupsInfo(object):
     def filter_visible_group_ids(self, gIds):
         visibleGroupIds = self.get_visible_group_ids()
         return [g for g in gIds if (g in visibleGroupIds)]
+
+    def get_member_groups_for_user(self, user, authenticatedUser):
+        """Get a list of all groups that the user is a member of.
+        
+        ARGUMENTS
+            user: A user instance to query.
+            authenticatedUser: The instance of the user who is requesting 
+                the information.
+        
+        RETURNS
+            A list of groups the user is a member of. If "user" is the
+            same as the "authenticatedUser", then the list will be of all
+            groups that "user" is a member of. Otherwise only the groups
+            that the user is a member of, and has posted to, will be 
+            listed.
+        
+        SIDE EFFECTS
+            None.
+        """
+        assert user
+        retval = []
+        if (user.getId() == authenticatedUser.getId()):
+            retval = self.__get_member_groups_for_user(user)
+        else:
+            retval = self.__get_visible_member_groups_for_user(user)
+        assert type(retval) == list
+        return retval
+
+    def __get_member_groups_for_user(self, user):
+        """Get a list of all groups the user is currently a member of.
+        
+        WARNING
+            Showing group-memberships to the wrong people may result in
+            a privacy breach. It is safer to use 
+            "get_member_groups_for_user"
+            
+        ARGUMENTS
+            user: A user instance.
+            
+        RETURNS
+            A list of all groups the user is currently a member of.
+        
+        SIDE EFFECTS
+            None.
+        """
+        assert user
+        visibleGroups = self.get_visible_groups()
+        retval = [g for g in visibleGroups
+                  if 'GroupMember' in user.getRolesInContext(g)]
+        assert type(retval) == list
+        return retval
+
+    def __get_visible_member_groups_for_user(self, user):
+        """Get the publicly visible groups that the user is a member of.
+        
+        ARGUMENTS
+            user: A user instance.
+            
+        RETURNS
+            A list of groups that the user is currently a member of, and
+            has posted to.
+        
+        SIDE EFFECTS
+            None.
+        """
+        assert user
+        retval = []
+        memberGroups = self.__get_member_groups_for_user(user)
+        sId = self.siteInfo.get_id()
+        uId = user.getId()
+        q = self.groupQuery
+        for g in memberGroups:
+            authors = [ar['user_id'] for ar in
+                        q.authors_posts_in_group(sId, g.getId())]
+            if uId in authors:
+                retval.append(g)
+        assert type(retval) == list
+        return retval
 
