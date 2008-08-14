6706f8df4fff03a9a72ef54ba626a7142b3e8478
Michael JasonSmith <mpj17@onlinegroups.net>
2008-08-14 02:13:50 +0000

    Allow access to the site info and group info classes from ZMI-side scripts.
    --HG--
    branch : GSContent
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSContent%404062

--- __init__.py
+++ __init__.py
@@ -1,15 +1,20 @@
 # This space intentionally left blank
-import groupsInfo, groupInfo, titleBar
+import groupsInfo, groupInfo, titleBar, view
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class
+
 module_security = ModuleSecurityInfo('Products.GSContent')
 groupInfo_security = ModuleSecurityInfo('Products.GSContent.groupInfo')
-from groupInfo import GSGroupInfoFactory
+from groupInfo import GSGroupInfoFactory, GSGroupInfo
 allow_class(GSGroupInfoFactory)
+allow_class(GSGroupInfo)
 
 groupsInfo_security = ModuleSecurityInfo('Products.GSContent.groupsInfo')
 from groupsInfo import GSGroupsInfoFactory, GSGroupsInfo
 allow_class(GSGroupsInfoFactory)
 allow_class(GSGroupsInfo)
 
+groupsInfo_security = ModuleSecurityInfo('Products.GSContent.view')
+from view import GSSiteInfo
+allow_class(GSSiteInfo)
 
