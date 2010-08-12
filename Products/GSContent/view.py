3876094c12857bfe5278198d62538c3878ae1244
Michael JasonSmith <mpj17@onlinegroups.net>
2010-08-12 14:41:30 +1000

    Removing some comments

--- Products/GSContent/view.py
+++ Products/GSContent/view.py
@@ -195,9 +195,6 @@ class GSSiteInfo:
 class GSSiteHomepageView(BrowserView):
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
-        # these two might be required in 2.10
-        #self.siteInfo = createObject('groupserver.SiteInfo', context.aq_self)
-        #self.groupsInfo = createObject('groupserver.GroupsInfo', context.aq_self)
         self.siteInfo = createObject('groupserver.SiteInfo', context)
         self.groupsInfo = createObject('groupserver.GroupsInfo', context)
 
@@ -210,9 +207,6 @@ class GSContentView(BrowserView):
     @property
     def siteInfo(self):
         if self.__siteInfo == None:
-            # this might be required in 2.10
-            #self.__siteInfo = createObject('groupserver.SiteInfo', 
-            #                    self.context.aq_self)
             self.__siteInfo = createObject('groupserver.SiteInfo', 
                                 self.context)
         assert self.__siteInfo
@@ -221,9 +215,6 @@ class GSContentView(BrowserView):
     @property
     def groupsInfo(self):
         if self.__groupsInfo == None:
-            # this might be required in 2.10
-            #self.__groupsInfo = createObject('groupserver.GroupsInfo', 
-            #                        self.context.aq_self)
             self.__groupsInfo = createObject('groupserver.GroupsInfo', 
                                     self.context)
         assert self.__groupsInfo
