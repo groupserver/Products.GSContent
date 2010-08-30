fcd0cb395adc5cba18e159296ef7ed6b3e65f12c
Richard Waid <richard@iopen.net>
2010-08-31 11:47:15 +1200

    Hack to allow stuff to allow legacy content to work

--- Products/GSContent/__init__.py
+++ Products/GSContent/__init__.py
@@ -1,9 +1,17 @@
 # This space intentionally left blank
 import titleBar, view
 from AccessControl import ModuleSecurityInfo
-from AccessControl import allow_class
+from AccessControl import allow_class, allow_module
 
 siteInfo_security = ModuleSecurityInfo('Products.GSContent.view')
 from view import GSSiteInfo
 allow_class(GSSiteInfo)
+
+# XXX: Dirty hack, purely to support legacy code. Remove as soon as possible
+def GSGroupsInfoFactory():
+    logger.warn("Deprecated: GSGroupsInfoFactory should be imported directly fr$
+    from gs.groups.groupsInfo import GSGroupsInfoFactory as GSGIFactory
+    return GSGIFactory()
+
+allow_module('Products.GSContent.GSGroupsInfoFactory')
 
