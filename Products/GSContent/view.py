8d7c124352cfc366b7372543feb8485189001748
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-27 10:22:10 -0800

    PEP-8 cleanup, so help me God.

--- Products/GSContent/view.py
+++ Products/GSContent/view.py
@@ -1,73 +1,87 @@
-# coding=utf-8
-'''GroupServer-Content View Class
-'''
-from interfaces import IGSSiteInfo
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from zope.component.interfaces import IFactory
 from zope.interface import implements, implementedBy
-
 from App.class_init import InitializeClass
-
 from Products.Five import BrowserView
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.XWFCore import XWFUtils
+from .interfaces import IGSSiteInfo
+
 
 class GSSiteInfoFactory(object):
     implements(IFactory)
-    
+
     title = u'GroupServer Site Info Factory'
     descripton = u'Create a new GroupServer site information instance'
-    
+
     def __call__(self, context):
         retval = GSSiteInfo(context)
         return retval
-        
+
     def getInterfaces(self):
         retval = implementedBy(GSSiteInfo)
         assert retval
         return retval
 
+
 class GSSiteInfo:
     """An implementation of the GroupServer Site Information Interface
-    
+
     This adaptor provides information about the site, based on the context
     of the object.
     """
-    implements( IGSSiteInfo )
+
+    implements(IGSSiteInfo)
+
     def __init__(self, context):
         """Create an GSSiteInfo instance.
-        
+
         ARGUMENTS
             "context" The context of the page.
-        
+
         SIDE EFFECTS
-            Sets 
-              * "self.context" to the context argument, 
-              * "self.siteObj" to the site-instance, and 
+            Sets
+              * "self.context" to the context argument,
+              * "self.siteObj" to the site-instance, and
               * "self.config" to the site-configuration instance.
         """
         self.context = context
         self.siteObj = self.__get_site_object()
         self.config = self.__get_site_config()
-        
+
     def __get_site_object(self):
         """Get the site-object, which contains the current instance.
-        
-        This method preforms a search for the site instance among the 
+
+        This method preforms a search for the site instance among the
         ancestors of the current object, walking back up the tree looking
         for the object that has the "is_division" property set to "True".
-        
+
         ARGUMENTS
             None.
-            
+
         RETURNS
             The site object.
 
         SIDE EFFECTS
             None.
-            
+
         ENVIRONMENT
-            "self.context": The context of the object.     
+            "self.context": The context of the object.
         """
         retval = self.context
         markerAttr = 'is_division'
@@ -85,19 +99,19 @@ class GSSiteInfo:
         except AttributeError:
             retval = None
         return retval
-                
+
     def __get_site_config(self):
         """ Get the site configuration instance from the site object.
-        
+
         ARGUMENTS
             None.
-        
+
         RETURNS
             The site configuration instance.
-            
+
         SIDE EFFECTS
             None.
-            
+
         ENVIRONMENT
             "self.siteObject": The site object.
 
@@ -106,21 +120,23 @@ class GSSiteInfo:
             retval = getattr(self.siteObj, 'DivisionConfiguration', None)
         else:
             retval = getattr(self.context, 'GlobalConfiguration', None)
-        
+
         return retval
 
-    @property
+    @Lazy
     def id(self):
         return self.get_id()
+
     def get_id(self):
         retval = None
         if self.siteObj:
             retval = self.siteObj.getId()
         return retval
-    
+
     @property
     def name(self):
-        return self.get_name()        
+        return self.get_name()
+
     def get_name(self):
         try:
             retval = self.config.getProperty('siteName', '')
@@ -136,12 +152,13 @@ class GSSiteInfo:
         if isinstance(retval, str):
             retval = unicode(retval, 'utf-8', 'ignore')
         return retval
-        
-    @property
+
+    @Lazy
     def url(self):
-        return self.get_url()        
+        return self.get_url()
+
     def get_url(self):
-        '''Gets the URL from the site. It is made up of the canonical 
+        '''Gets the URL from the site. It is made up of the canonical
         host and the canonical port. If the canonical port is not set,
         or is set to port 80, then it is not included in the URL.'''
         retval = '/'
@@ -160,12 +177,13 @@ class GSSiteInfo:
     @property
     def site_admins(self):
         return self.get_site_admins()
+
     def get_site_admins(self):
         admins = self.siteObj.users_with_local_role('DivisionAdmin')
-        retval = [ createObject('groupserver.UserFromId', 
-                      self.context, a) for a in admins ]
+        retval = [createObject('groupserver.UserFromId',
+                      self.context, a) for a in admins]
         return retval
-        
+
     @property
     def showImage(self):
         return self.config.getProperty('showImage', False)
@@ -178,61 +196,63 @@ class GSSiteInfo:
         retval = '/'.join(self.siteObj.getPhysicalPath())
         assert len(retval) >= 1
         return retval
-        
+
     def get_support_email(self):
         retval = XWFUtils.get_support_email(self.context, self.get_id())
         assert type(retval) == str
         assert retval
         assert '@' in retval
         return retval
-        
+
     def get_property(self, prop, default=None):
         assert self.siteObj, 'Site instance does not exist\n'\
           'Context %s\nID %s' % (self.context, self.id)
         retval = self.siteObj.getProperty(prop, default)
         if isinstance(retval, str):
             retval = unicode(retval, 'utf-8', 'ignore')
-        
+
         return retval
-        
+
+
 class GSContentView(BrowserView):
     '''View object for standard GroupServer content objects'''
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
         self.__siteInfo = self.__groupsInfo = None
-    
+
     @property
     def siteInfo(self):
-        if self.__siteInfo == None:
-            self.__siteInfo = createObject('groupserver.SiteInfo', 
+        if self.__siteInfo is None:
+            self.__siteInfo = createObject('groupserver.SiteInfo',
                                 self.context)
         assert self.__siteInfo
         return self.__siteInfo
-        
+
     @property
     def groupsInfo(self):
-        if self.__groupsInfo == None:
-            self.__groupsInfo = createObject('groupserver.GroupsInfo', 
+        if self.__groupsInfo is None:
+            self.__groupsInfo = createObject('groupserver.GroupsInfo',
                                     self.context)
         assert self.__groupsInfo
         return self.__groupsInfo
-        
+
     def process_form(self):
         form = self.context.REQUEST.form
         result = {}
-        if form.has_key('submitted'):
+        if 'submitted' in form:
             submit = form.get('__submit__')
             model, instance = submit.split('+')
             model = form.get('model_override', model)
-            
+
             site_root = self.context.site_root()
 
             localScripts = getattr(site_root.LocalScripts, 'forms', None)
             oldScripts = getattr(site_root.Scripts, 'forms')
             assert oldScripts, 'Could not get folder Scripts/forms'
-             
-            modelDir = localScripts and getattr(localScripts, model, 
-              getattr(oldScripts, model, None)) or getattr(oldScripts, model, None)
+
+            modelDir = localScripts and getattr(localScripts, model,
+                                          getattr(oldScripts, model, None)) \
+                          or getattr(oldScripts, model, None)
             if modelDir:
                 assert hasattr(modelDir, model)
                 if hasattr(modelDir, instance):
@@ -247,18 +267,17 @@ class GSContentView(BrowserView):
                     result['error'] = True
                     result['message'] = m
             else:
-                m = """<p>Could not find the model 
+                m = """<p>Could not find the model
                        <code>%s</code>.</p>""" % model
                 result['error'] = True
                 result['message'] = m
-            assert result.has_key('error')
-            assert result.has_key('message')
+            assert 'error' in result
+            assert 'message' in result
             assert result['message'].split
-    
-        result['form'] = form            
+
+        result['form'] = form
         return result
 
     # To be converted: Scripts.get_firstLevelFolder(context)
 
-InitializeClass( GSContentView )
-
+InitializeClass(GSContentView)
