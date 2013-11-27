d76469b8d2413bb5531ccbf7dd07e2df3e1166ab
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-27 10:21:33 -0800

    Egg metadata fix.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from setuptools import setup, find_packages
 import os
 
@@ -10,11 +23,11 @@ setup(name='Products.GSContent',
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
-        "Development Status :: 4 - Beta",
+        'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
         "Framework :: Zope2",
         "Intended Audience :: Developers",
-        "License :: Other/Proprietary License",
+        'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux"
         "Programming Language :: Python",
@@ -24,7 +37,7 @@ setup(name='Products.GSContent',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
       url='http://groupserver.org',
-      license='ZPL',
+      license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
