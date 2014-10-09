320aea83a3bfbfed8b5d101e4760390695a91a9d
Michael JasonSmith <mpj17@onlinegroups.net>
2014-10-09 15:55:15 -0700

    Updating the product metadata

--- README.rst
+++ README.rst
@@ -1,9 +1,39 @@
+======================
+``Products.GSContent``
+======================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"Content" pages for GroupServer
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Michael JasonSmith`_
+:Contact: Michael JasonSmith <mpj17@onlinegroups.net>
+:Date: 2014-10-09
+:Organization: `GroupServer.org`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 4.0 International License`_
+  by `OnlineGroups.Net`_.
+
 Introduction
 ============
 
-The code in this product is responsible for displaying the content-pages
-on GroupServer: what we call the "pragmatic templates". It controls
-the basic layout of the XHTML 1.0 pages, and the JavaScript that we
-use everywhere.
+The code in this product is responsible for displaying the
+content-pages on GroupServer: what we call the "pragmatic
+templates". It controls the basic layout of the XHTML 1.0 pages,
+and the JavaScript that we use everywhere.
 
-It is slowly being split up into the smaller ``gs.content.*`` products.
+It is slowly being split up into the smaller ``gs.content.*``
+products.
+
+Resources
+=========
+
+- Code repository: https://github.com/groupserver/Products.GSContent
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/groupserver
+
+.. _GroupServer: http://groupserver.org/
+.. _GroupServer.org: http://groupserver.org/
+.. _OnlineGroups.Net: https://onlinegroups.net
+.. _Michael JasonSmith: http://groupserver.org/p/mpj17
+.. _Creative Commons Attribution-Share Alike 4.0 International License:
+    http://creativecommons.org/licenses/by-sa/4.0/
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
+# OnlineGroups.net and Contributors.
+#
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -11,32 +13,40 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 from setuptools import setup, find_packages
+import codecs
 import os
-
 from version import get_version
 
+
+version = get_version()
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='Products.GSContent',
-      version=get_version(),
+      version=version,
       description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      long_description=long_description,
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux"
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
-      url='http://groupserver.org',
+      url='https://github.com/groupserver/Products.GSContent',
       license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
